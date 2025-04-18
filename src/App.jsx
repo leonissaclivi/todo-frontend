bb2732ae0ac6b17a831e9b85e90f6acbe9f4a2b6
Leon <leonissaclivi@gmail.com>
2025-04-19 00:35:21 +0530

    Fix routing and default to login page

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,7 @@ function App() {
         <Route path='/login' element={<Login/>}/>
         <Route path='/signup' element={<Singup/>}/>
         <Route path='/todos' element={<Todos todos={todos}/>}/>
+        <Route path="/" element={<Navigate to="/login" replace />} />
         </Routes>
       </Router>
     </div>
